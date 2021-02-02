ebb257ca5078478e62a8f9e84595ca738c43bb5a
Lactea98 <whdals7979@naver.com>
2021-02-02 21:07:00 +0900

    discord bot init

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+const discord = require("./bot");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) =>{
+    discord.sendBot("title\ntest", "\`message\`");
     res.render("index");
 })
 
--- bot.js
+++ bot.js
@@ -1,6 +1,24 @@
 const Discord = require('discord.js');
+const config = require('./config/token.json');
+
 const client = new Discord.Client();
-const token = require('./config/token.json'); //token instance
+const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken)
+
+client.login(config.token);
+
+const sendBot = (title, message) => {
+    const embed = new Discord.MessageEmbed()
+    .setTitle(title)
+    .setColor('#0099ff');
+
+    webhookClient.send(message, {
+        username: 'Casper the Friendly Ghost', //
+        avatarURL: 'https://www.casper.or.kr/xe/files/attach/xeicon/favicon.ico',
+        embeds: [embed],
+    });
+}
+exports.sendBot = sendBot;
+
 
 client.on('ready', function(){
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,4 +30,5 @@ client.on('message', function(msg){
     }
 });
 
-client.login(token.token); //token login
+
+client.login(config.token); //token login
