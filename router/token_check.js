248e08710a88084ad339a9fd9f47ff92baee3eed
Lactea98 <whdals7979@naver.com>
2021-02-04 15:48:44 +0900

    bug fix

--- router/token_check.js
+++ router/token_check.js
@@ -4,10 +4,10 @@ const sessionCheck = require("../feature/sessionCheck");
 const config = require("../config/token.json");
 
 router.post("/token_check", (req, res) => {
-    console.log(req.body.token);
     if (req.body.token == config.casper_check_token) {
         req.session.token_check_ok = "true";
         res.redirect('/form');
+        return;
     }
 
     res.send("<script>alert('틀림'); location.href='/'; </script>");
