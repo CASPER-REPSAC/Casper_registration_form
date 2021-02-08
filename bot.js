33a23e8f41be2899c275b00008122b5a28e0a6ae
Floodnut <gsniper777@gmail.com>
2021-02-08 23:11:22 +0900

    DB 조회기능 추가

--- bot.js
+++ bot.js
@@ -21,14 +21,37 @@ const sendBot = (title, message) => {
     });
 }
 
+function sliceByByte(str, maxByte) {
+	for(b=i=0;c=str.charCodeAt(i);) { 
+      b+=c>>7?2:1;
+      if (b > maxByte)
+      break;
+      i++;    
+    }
+  	return str.substring(0,i);
+}
+function Rpad(str,padLen,padStr){
+    if (padStr.length > padLen) {
+        console.log("오류 : 채우고자 하는 문자열이 요청 길이보다 큽니다");
+        return str + "";
+    }
+    str += ""; // 문자로
+    padStr += ""; // 문자로
+    while (str.length < padLen)
+        str += padStr;
+    str = str.length >= padLen ? str.substring(0, padLen) : str;
+    return str;
+}
+
+
 client.on('ready', function(){
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', function(msg){
-    if(msg.content[0] === '>'){ // '>' is command
+    if(msg.content[0] === '>'){ //'msg.content' is command in this instance
         if(msg.content.slice(1)==='help'){
-            msg.reply(">apply : nas, wiki 미승인 계정 확인\n\t옵션 -d [True/False] : 미승인 계정 승인 여부 변경\n");
+            msg.reply("\`명령어 목록\n┌───────┬────────────────────────────────┬───────┐\n│help   │Print Commands List             │Option │\n│apply  │Find unauthorized accounts      │d      │\n└───────┴────────────────────────────────┴───────┘\`"); //50
         }
         if(msg.content.slice(1)==='apply'){
             MongoClient.connect("mongodb://127.0.0.1:27017/", (err,db)=>{
@@ -36,10 +59,17 @@ client.on('message', function(msg){
                 alarmDB.collection("users", (err, users)=>{
                     users.find((err,items)=>{
                         items.toArray((err,itemArr)=>{
-                            const messageD = itemArr.toString();
+                            let messageApply= '\`미승인 계정 목록\n┌──────────────┬───────────────┬─────┬─────┬─────┐\n│username      │userid         │nas  │wiki │auth │\n├──────────────┼───────────────┼─────┼─────┼─────┤\n';
                             console.log(itemArr);
-                            console.log(messageD);
-                            msg.reply(messageD, "미승인 목록");
+                            for(let i = 0;i<itemArr.length;i++){
+                                console.log('username: '+itemArr[i].username +'    userid: '+itemArr[i].userid);
+                                messageApply = messageApply + '│'+ sliceByByte(Rpad(itemArr[i].username,14,'  '),14) +'│'+ Rpad(itemArr[i].userid,15,' ')+'│'+Rpad(itemArr[i].nas,5,' ')+'│'+Rpad(itemArr[i].wiki,5,' ')+'│'+Rpad(itemArr[i].done,5,' ')+'│'+'\n';
+                                if(i === 8){
+                                    messageApply = messageApply + '└──────────────┴───────────────┴─────┴─────┴─────┘\`'
+                                    msg.reply(messageApply);
+                                    console.log(messageApply);
+                                }
+                            }
                         });
                     });                    
                 });
